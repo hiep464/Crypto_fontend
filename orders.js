4f453164c70572eadd84ec3fa0839c4be3b824d6
Luu Hiep <luuhiep2309@gmail.com>
2022-08-23 22:56:37 +0700

    first commit

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 pro Drone49347',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '96996',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'GPS 4k Drone22821',
+        productNumber: '85631',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
